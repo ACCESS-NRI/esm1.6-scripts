8fd3a8d4d2916e588d14ce1b33d1609dfa41359e
Joshua Torrance <joshua.torrance@anu.edu.au>
2026-04-23 10:09:07 +1000

    Added a simple test for automatic field determination.

--- splitnc/test/test_splitnc.py
+++ splitnc/test/test_splitnc.py
@@ -4,6 +4,8 @@ import re
 import xarray as xr
 
 from common import runcmd, make_nc
+from splitnc import determine_field_vars
+
 
 @pytest.mark.parametrize(
     "cdl_file,cmd_options,field_regex",
@@ -34,7 +36,10 @@ from common import runcmd, make_nc
         ),
     ]
 )
-def test_splitting(tmp_path, cdl_file, cmd_options, field_regex):
+def test_splitnc(tmp_path, cdl_file, cmd_options, field_regex):
+    """
+    Test running splitnc from the command line
+    """
     # Create a file to test on
     ncfile = make_nc(tmp_path, f"test/data/{cdl_file}")
 
@@ -54,3 +59,53 @@ def test_splitting(tmp_path, cdl_file, cmd_options, field_regex):
                 count += 1
 
         assert count == 1
+
+@pytest.mark.parametrize(
+    "cdl_file,field_regex",
+    [
+        (
+            # Test a simple cdl
+            "simple.cdl",
+            "field",
+        ),
+        (
+            # Test a simple cdl that has co-dependent fields - i.e. none will be detected
+            "simple_circular.cdl",
+            "none",
+        ),
+        (
+            # Test a monthly atmosphere file - will also pick up latitude_longitude
+            "aiihca.pa-234501_mon.cdl",
+            "fld_.+|latitude_longitude",
+        ),
+        (
+            # Test a daily atmosphere file - will also pick up latitude_longitude
+            "aiihca.pe-234501_dai.cdl",
+            "fld_.+|latitude_longitude",
+        ),
+        (
+            # Test a monthly ice file - will also pick up some extra fields
+            "iceh-1monthly-mean_2345-01.cdl",
+            "(ai|dv|si||tarea|tmask|uarea|VGRD).*",
+        ),
+        (
+            # Test a daily ice file - will also pick up some extra fields
+            "iceh-1daily-mean_2345-01.cdl",
+            "(ai|dv|si||tarea|tmask|uarea|VGRD).*",
+        ),
+    ]
+)
+def test_determine_field_vars(tmp_path, cdl_file, field_regex):
+    """
+    Test the functionality for the automatic determinations of field vars
+    """
+    # Create a file to test on
+    ncfile = make_nc(tmp_path, f"test/data/{cdl_file}")
+
+    decoder = xr.coders.CFDatetimeCoder(use_cftime=True)
+    with xr.open_dataset(ncfile, decode_times=decoder) as ds:
+        field_list = determine_field_vars(ds)
+
+        # Check all the discovered fields match the regex
+        print(field_list)
+        assert all([re.match(field_regex, v) for v in field_list])
